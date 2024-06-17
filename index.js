059abc1d5edb542d088f0354964e98bafa855feb
atuligt <159754755+atuligt@users.noreply.github.com>
2024-06-17 11:09:24 +0530

    Add debugging

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const clientRoutes = require("./src/routes/clientRoutes/fetchImage.routes.js");
 const bodyParser = require("body-parser");
 const path = require("path");
 const cors = require("cors");
+const fs = require('fs');
 require("dotenv").config();
 app.use(cors());
 app.use(bodyParser.json());
@@ -12,8 +13,17 @@ app.use(express.static("uploads"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.get("/pingTest", (req, res) => {
   const uploadsPath = path.join(__dirname, "uploads");
-
+  const imagesFolderPath = path.join(uploadsPath, 'images');
+  const imagesPath = path.join(__dirname, "images");
+  // Check if the 'images' folder exists, create it if it doesn't
+  if (!fs.existsSync(imagesFolderPath)) {
+      fs.mkdirSync(imagesFolderPath, { recursive: true });
+      console.log('Images folder created successfully.');
+  } else {
+      console.log('Images folder already exists.');
+  }
   console.log("Uploads folder path:", uploadsPath);
+  console.log("Images folder path", imagesPath);
   return res.status(200).json({ message: "In index call" });
 });
 app.use("/admin", AdminRoutes);
