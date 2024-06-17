d500036cf18b38d050583646cba5d76c01c31b78
atuligt <159754755+atuligt@users.noreply.github.com>
2024-06-17 11:15:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const clientRoutes = require("./src/routes/clientRoutes/fetchImage.routes.js");
 const bodyParser = require("body-parser");
 const path = require("path");
 const cors = require("cors");
-const fs = require('fs');
+const fs = require("fs");
 require("dotenv").config();
 app.use(cors());
 app.use(bodyParser.json());
@@ -13,14 +13,16 @@ app.use(express.static("uploads"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.get("/pingTest", (req, res) => {
   const uploadsPath = path.join(__dirname, "uploads");
-  const imagesFolderPath = path.join(uploadsPath, 'images');
   const imagesPath = path.join(__dirname, "images");
   // Check if the 'images' folder exists, create it if it doesn't
+  const uploadsFolderPath = "/var/task/uploads"; // Adjust the path as per your specific environment
+
+  const imagesFolderPath = path.join(uploadsFolderPath, "images");
   if (!fs.existsSync(imagesFolderPath)) {
-      fs.mkdirSync(imagesFolderPath, { recursive: true });
-      console.log('Images folder created successfully.');
+    fs.mkdirSync(imagesFolderPath, { recursive: true });
+    console.log("Images folder created successfully.");
   } else {
-      console.log('Images folder already exists.');
+    console.log("Images folder already exists.");
   }
   console.log("Uploads folder path:", uploadsPath);
   console.log("Images folder path", imagesPath);
