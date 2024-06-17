46d1f01f85f07e3ac83c3d4588d96a95dcfc1faf
atuligt <159754755+atuligt@users.noreply.github.com>
2024-06-17 11:46:21 +0530

    Update adminRoutes upload function

--- src/routes/adminRoutes/uploadImage.routes.js
+++ src/routes/adminRoutes/uploadImage.routes.js
@@ -4,35 +4,42 @@ const multer = require("multer");
 const path = require("path");
 const ROUTES = express.Router();
 
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null,  path.join(__dirname, "../../../uploads/images"));
-  },
-  filename: function (req, file, cb) {
-    cb(
-      null,
-      file.fieldname + "-" + Date.now() + path.extname(file.originalname)
-    );
-  },
-});
-function checkFileType(file, cb) {
-  const filetypes = /jpeg|jpg|png/;
-  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
-  const mimetype = filetypes.test(file.mimetype);
+// const storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     cb(null,  path.join(__dirname, "../../../uploads/images"));
+//   },
+//   filename: function (req, file, cb) {
+//     cb(
+//       null,
+//       file.fieldname + "-" + Date.now() + path.extname(file.originalname)
+//     );
+//   },
+// });
+// function checkFileType(file, cb) {
+//   const filetypes = /jpeg|jpg|png/;
+//   const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+//   const mimetype = filetypes.test(file.mimetype);
 
-  if (mimetype && extname) {
-    return cb(null, true);
-  } else {
-    cb("Error: Images only! (jpeg, jpg, png)");
-  }
-}
+//   if (mimetype && extname) {
+//     return cb(null, true);
+//   } else {
+//     cb("Error: Images only! (jpeg, jpg, png)");
+//   }
+// }
+// const upload = multer({
+//   storage: storage, 
+//   fileFilter: function (req, file, cb) {
+//     checkFileType(file, cb);
+//   },
+// });
 const upload = multer({
-  storage: storage, 
-  fileFilter: function (req, file, cb) {
-    checkFileType(file, cb);
-  },
+  storage: multer.diskStorage({
+      destination: '../../../uploads/images', // Uploads directory in your Vercel project
+      filename: function (req, file, cb) {
+          cb(null, file.originalname); // Use the original filename
+      }
+  })
 });
-
 ROUTES.post("/uploadImage", upload.single("image"), uploadImage);
 
 module.exports = ROUTES;
