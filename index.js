d1cc9f3e83cfcfb948d1d7fa3786cfb37ba0ff48
atuligt <159754755+atuligt@users.noreply.github.com>
2024-06-17 11:03:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,19 +3,22 @@ const app = express();
 const AdminRoutes = require("./src/routes/adminRoutes/uploadImage.routes.js");
 const clientRoutes = require("./src/routes/clientRoutes/fetchImage.routes.js");
 const bodyParser = require("body-parser");
-const path = require('path');
-const cors = require('cors');
-require('dotenv').config();
+const path = require("path");
+const cors = require("cors");
+require("dotenv").config();
 app.use(cors());
 app.use(bodyParser.json());
 app.use(express.static("uploads"));
 app.use(bodyParser.urlencoded({ extended: true }));
-app.get("/pingTest",(req, res)=>{
-        return res.status(200).json({ message:"In index call"});
-})
-app.use('/admin', AdminRoutes);
-app.use('/client', clientRoutes);
+app.get("/pingTest", (req, res) => {
+  const uploadsPath = path.join(__dirname, "uploads");
+
+  console.log("Uploads folder path:", uploadsPath);
+  return res.status(200).json({ message: "In index call" });
+});
+app.use("/admin", AdminRoutes);
+app.use("/client", clientRoutes);
 
 app.listen(8000, () => {
-    console.info(`Server is listening at port 8000`);
-})
+  console.info(`Server is listening at port 8000`);
+});
